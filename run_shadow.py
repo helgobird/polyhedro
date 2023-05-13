f698db66af4c077e8db00bf8c41fb1729f950070
helgobird <130367852+helgobird@users.noreply.github.com>
2023-05-13 17:39:39 +0300

    Update run_shadow.py

--- run_shadow.py
+++ run_shadow.py
@@ -7,14 +7,18 @@ from shadow.polyedr import Polyedr
 
 tk = TkDrawer()
 try:
-    for name in ["ccc", "cube", "box", "king", "cow"]:
+    for name in ["tri_hor", "ccc", "cube", "box", "king", "cow"]:
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
-        Polyedr(f"data/{name}.geom").draw(tk)
+        pol = Polyedr(f"data/{name}.geom")
+        pol.draw(tk)
+        pol.ihwfunc()
         delta_time = time() - start_time
+        print("Сумма площадей проеций граней,", end=' ')
+        print(f"удовлетворяющих условию задачи, равно {pol.oxyarea}")
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
         input("Hit 'Return' to continue -> ")
-except(EOFError, KeyboardInterrupt):
+except (EOFError, KeyboardInterrupt):
     print("\nStop")
     tk.close()
